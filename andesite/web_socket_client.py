a5a5e45c6734232d4b9ff5ae547995312039a424
Simon Berger <siku2@outlook.com>
2019-03-07 22:58:06 +0100

    docs for life

--- andesite/web_socket_client.py
+++ andesite/web_socket_client.py
@@ -1,3 +1,5 @@
+"""Web socket client for Andesite."""
+
 import asyncio
 import logging
 import math
@@ -14,19 +16,34 @@ from .event_target import Event, EventTarget
 from .models import Equalizer, FilterUpdate, Karaoke, Operation, Play, Timescale, Tremolo, Update, Vibrato, VoiceServerUpdate, VolumeFilter
 from .transform import convert_to_raw, map_convert_values_to_milli, map_filter_none, to_milli
 
-__all__ = ["AndesiteWebSocket", "WebsocketMessageEvent", "WebsocketSendEvent"]
+__all__ = ["AndesiteWebSocket", "WebSocketMessageEvent", "WebSocketSendEvent"]
 
 log = logging.getLogger(__name__)
 
 
-class WebsocketMessageEvent(Event):
+class WebSocketMessageEvent(Event):
+    """Event emitted when a web socket message is received.
+
+    Attributes:
+        body: Raw body of the received message.
+            Note: The body isn't manipulated in any way other
+                than being loaded from the raw JSON string.
+                For example, the names are still in dromedaryCase.
+    """
     body: Dict[str, Any]
 
     def __init__(self, body: Dict[str, Any]) -> None:
         super().__init__("ws_message", body=body)
 
 
-class WebsocketSendEvent(Event):
+class WebSocketSendEvent(Event):
+    """Event dispatched before a web socket message is sent.
+
+    Attributes:
+        guild_id: guild id
+        op: Op-code to be executed
+        body: Raw body of the message
+    """
     guild_id: int
     op: str
     body: Dict[str, Any]
@@ -36,6 +53,18 @@ class WebsocketSendEvent(Event):
 
 
 async def try_connect(uri: str, **kwargs) -> Optional[WebSocketClientProtocol]:
+    """Connect to the given uri and return the client.
+
+    Catches exceptions which could potentially be solved
+    by retrying.
+
+    Args:
+        uri: URI to connect to
+        **kwargs: keyword arguments to pass to the `websockets.connect` function.
+
+    Returns:
+        `WebSocketClientProtocol` if the connection succeeded, None otherwise.
+    """
     try:
         client = await websockets.connect(uri, **kwargs)
     except InvalidHandshake as e:
@@ -83,6 +112,10 @@ class AndesiteWebSocket(EventTarget):
                 If this is not set, `AndesiteWebSocket.max_connect_attempts` is used instead.
                 If `None` unlimited attempts will be performed.
 
+        Raises:
+            `ValueError`: If the client is already connected.
+                Check `AndesiteWebSocket.connected` first!
+
         Notes:
             This method doesn't have to be called manually,
             it is called as soon as the first message needs to be sent.
@@ -121,7 +154,15 @@ class AndesiteWebSocket(EventTarget):
         await self._replay_message_queue()
 
     async def disconnect(self) -> None:
-        """Disconnect the web socket."""
+        """Disconnect the web socket.
+
+        This will obviously stop the client from
+        receiving events from Andesite.
+
+        Raises:
+            `ValueError`: If the client isn't connected.
+                Check `AndesiteWebSocket.connected` first!
+        """
         if not self.connected:
             raise ValueError("Not connected")
 
@@ -172,7 +213,7 @@ class AndesiteWebSocket(EventTarget):
             except JSONDecodeError:
                 raise
 
-            _ = self.dispatch(WebsocketMessageEvent(data))
+            _ = self.dispatch(WebSocketMessageEvent(data))
 
     def _start_read_loop(self) -> None:
         if self._read_loop and not self._read_loop.done():
@@ -190,10 +231,13 @@ class AndesiteWebSocket(EventTarget):
         """Send a payload.
 
         The guild_id and op are written to the payload before it is converted to JSON.
+
+        Regardless of whether the payload was actually sent or
+        not a `ws_send` event is dispatched.
         """
         payload.update(guildId=str(guild_id), op=op)
 
-        _ = self.dispatch(WebsocketSendEvent(guild_id, op, payload))
+        _ = self.dispatch(WebSocketSendEvent(guild_id, op, payload))
 
         log.debug(f"sending payload: {payload}")
 
@@ -210,6 +254,7 @@ class AndesiteWebSocket(EventTarget):
         """Send an `Operation`."""
         await self.send(guild_id, operation.__op__, convert_to_raw(operation))
 
+    # noinspection PyOverloads
     @overload
     async def play(self, guild_id: int, track: Play) -> None:
         ...
@@ -239,7 +284,6 @@ class AndesiteWebSocket(EventTarget):
                 If you pass a `Play` operation you must not set any of the
                 keyword arguments.
 
-        Keyword Args:
             start: timestamp, in seconds, to start the track
             end: timestamp, in seconds, to end the track
             pause: whether or not to pause the player
@@ -257,16 +301,42 @@ class AndesiteWebSocket(EventTarget):
         await self.send(guild_id, "play", payload)
 
     async def pause(self, guild_id: int, pause: bool) -> None:
+        """Pause a player.
+
+        If the player is already paused or unpaused,
+        this is a no-op.
+
+        Args:
+            guild_id: ID of the guild for which to pause
+            pause: `True` to pause, `False` to unpause
+        """
         await self.send(guild_id, "pause", dict(pause=pause))
 
     async def stop(self, guild_id: int) -> None:
+        """Stop a player.
+
+        Args:
+            guild_id: ID of the guild for which to stop
+        """
         await self.send(guild_id, "stop", {})
 
     async def seek(self, guild_id: int, position: float) -> None:
+        """Seek to a position.
+
+        Args:
+            guild_id: ID of the guild for which to seek
+            position: Timestamp, in seconds, to seek to
+        """
         payload = dict(position=to_milli(position))
         await self.send(guild_id, "seek", payload)
 
     async def volume(self, guild_id: int, volume: float) -> None:
+        """Set a player's volume.
+
+        Args:
+            guild_id: ID of the guild for which to set the volume
+            volume: Volume to set
+        """
         payload = dict(volume=to_milli(volume))
         await self.send(guild_id, "volume", payload)
 
@@ -287,6 +357,21 @@ class AndesiteWebSocket(EventTarget):
                      position: float = None,
                      volume: float = None,
                      filters: FilterUpdate = None) -> None:
+        """Send an update.
+
+        You may either provide the given keyword arguments,
+        or pass an `Update` operation which will be used instead.
+
+        Args:
+            guild_id: ID of the guild for which to update.
+            update: You can provide an `Update` operation instead of
+                the keyword arguments.
+
+            pause: whether or not to pause the player
+            position: timestamp to set the current track to, in seconds
+            volume: volume to set on the player
+            filters: configuration for the filters
+        """
         if isinstance(update, Update):
             payload = convert_to_raw(update)
         else:
@@ -300,9 +385,21 @@ class AndesiteWebSocket(EventTarget):
         await self.send(guild_id, "update", payload)
 
     async def destroy(self, guild_id: int) -> None:
+        """Destroy a player.
+
+        Args:
+            guild_id: ID of the guild for which to destroy the player
+        """
         await self.send(guild_id, "destroy", {})
 
     async def mixer(self, guild_id: int, enable: bool = None, **players: Union[Play, Update]) -> None:
+        """Configure the mixer player.
+
+        Args:
+            guild_id: ID of the guild for which to configure the mixer
+            enable: If present, controls whether or not the mixer should be used
+            **players: Map of player id to `Play` / `Update` payloads for each mixer source
+        """
         payload: Dict[str, Any] = convert_to_raw(players)
         payload["enable"] = enable
 
@@ -329,6 +426,20 @@ class AndesiteWebSocket(EventTarget):
                       tremolo: Tremolo = None,
                       vibrato: Vibrato = None,
                       volume: VolumeFilter = None) -> None:
+        """Configure the filters of a player.
+
+        Args:
+            guild_id: ID of the guild for which to configure the filters
+            filter_update: Instead of specifying the other keyword arguments
+                you may provide a `FilterUpdate` operation which will be used
+                instead
+            equalizer: Equalizer filter settings
+            karaoke: Karaoke filter settings
+            timescale: Timescale filter settings
+            tremolo: Tremolo filter settings
+            vibrato: Vibrato filter settings
+            volume: Volume filter settings
+        """
         if isinstance(filter_update, FilterUpdate):
             payload = convert_to_raw(filter_update)
         else:
@@ -353,7 +464,7 @@ class AndesiteWebSocket(EventTarget):
 
         await self.send(guild_id, "ping", {"ping": True})
 
-        def check_ping_response(event: WebsocketMessageEvent) -> bool:
+        def check_ping_response(event: WebSocketMessageEvent) -> bool:
             return event.body.get("ping", False)
 
         await self.wait_for("ws_message", check=check_ping_response)
@@ -361,14 +472,26 @@ class AndesiteWebSocket(EventTarget):
         return time.time() - start
 
     @overload
-    async def voice_server_update(self, guild_id: int, update: VoiceServerUpdate) -> None:
+    async def voice_server_update(self, update: VoiceServerUpdate) -> None:
+        """Provide a voice server update.
+
+        Args:
+            update: `VoiceServerUpdate` to use.
+        """
         ...
 
     @overload
     async def voice_server_update(self, guild_id: int, session_id: str, event: Dict[str, Any]) -> None:
+        """Provide a voice server update.
+
+        Args:
+            guild_id: ID of the guild of the voice server update
+            session_id: session id
+            event: voice server update event as sent by Discord
+        """
         ...
 
-    async def voice_server_update(self, guild_id: int, *args, **kwargs) -> None:
+    async def voice_server_update(self, *args, **kwargs) -> None:
         """Provide a voice server update."""
 
         # Because we want to preserve the names as shown in the
@@ -376,15 +499,15 @@ class AndesiteWebSocket(EventTarget):
         # that manually would be messy we just let Python do it.
 
         def extract_update(update: VoiceServerUpdate):
-            return convert_to_raw(update)
+            return update.guild_id, convert_to_raw(update)
 
-        def extract_session_and_event(session_id: str, event: Dict[str, Any]):
-            return dict(sessionId=session_id, event=event)
+        def extract_session_and_event(guild_id: int, session_id: str, event: Dict[str, Any]):
+            return guild_id, dict(sessionId=session_id, event=event)
 
         try:
             # this is probably more likely
-            payload = extract_session_and_event(*args, **kwargs)
+            guild_id, payload = extract_session_and_event(*args, **kwargs)
         except TypeError:
-            payload = extract_update(*args, **kwargs)
+            guild_id, payload = extract_update(*args, **kwargs)
 
         await self.send(guild_id, "voice-server-update", payload)
