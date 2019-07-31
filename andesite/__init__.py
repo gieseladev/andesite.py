1ad4d530b6f9d740c7f5488fc62360c800ca5697
Simon Berger <simon@siku2.io>
2019-07-31 23:03:26 +0200

    redo proper order

--- andesite/__init__.py
+++ andesite/__init__.py
@@ -11,11 +11,14 @@ Attributes:
 # must be defined before http client is imported
 __version__ = "0.2.1"
 
-from .combined_client import *  # 4
-from .http_client import *  # 2
 # preserve order:
 from .models import *  # 1
-from .pools import *  # 5
+
+from .http_client import *  # 2
+
 from .state import *  # 3.1
 from .web_socket_client import *  # 3.2
 from .web_socket_client_events import *  # 3.3
+
+from .combined_client import *  # 4
+from .pools import *  # 5
